9bfcc888b28db64ed73787e515b1563caa63a50e
woxie <wox2@seznam.cz>
2012-08-14 22:46:14 +0200

    modified validator tests, modified gpull script

--- scripts/gpull.sh
+++ scripts/gpull.sh
@@ -2,5 +2,23 @@
 git pull
 ECLIPSE=`which eclipse`
 D_ECLIPSE=`dirname $ECLIPSE`
-cp plugins/migdb.mm.app_1.0.0.jar $D_ECLIPSE/dropins
-cp plugins/migdb.mm.rdb_1.0.0.jar $D_ECLIPSE/dropins
+
+#conditional copying mm.app
+diff -q plugins/migdb.mm.app_1.0.0.jar $D_ECLIPSE/dropins/migdb.mm.app_1.0.0.jar
+if [ $? -ne 0 ]
+  then 
+	echo copying mm app
+	cp plugins/migdb.mm.app_1.0.0.jar $D_ECLIPSE/dropins
+  else
+     echo mm app is actual
+fi
+
+#conditional copying mm.rdb
+diff -q plugins/migdb.mm.rdb_1.0.0.jar $D_ECLIPSE/dropins/migdb.mm.rdb_1.0.0.jar
+if [ $? -ne 0 ]
+  then 
+	echo copying mm rdb
+	cp plugins/migdb.mm.rdb_1.0.0.jar $D_ECLIPSE/dropins
+  else
+     echo mm rdb is actual
+fi
