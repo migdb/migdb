ceffe3597dcb66190b08fe472c379572867c7bdb
woxie <wox2@seznam.cz>
2012-08-26 20:33:14 +0200

    added gexport.sh

--- scripts/gexport.sh
+++ scripts/gexport.sh
@@ -3,21 +3,21 @@
 #. `dirname $0`/config.cfg
 . ~/migdb_config.cfg
 
-#conditional copying mm.app
+#conditional exporting mm.app
 diff -q $D_MIGDB/plugins/migdb.mm.app_1.0.0.jar $D_ECLIPSE/dropins/migdb.mm.app_1.0.0.jar
 if [ $? -ne 0 ]
   then 
-	echo copying mm app
-	cp $D_MIGDB/plugins/migdb.mm.app_1.0.0.jar $D_ECLIPSE/dropins
+	echo exporting mm app
+	cp $D_ECLIPSE/dropins/migdb.mm.app_1.0.0.jar $D_MIGDB/plugins
   else
      echo mm app is actual
 fi
 
-#conditional copying mm.rdb
-diff -q $D_MIGDB/plugins/migdb.mm.rdb_1.0.0.jar $D_ECLIPSE/dropins/migdb.mm.rdb_1.0.0.jar
+#conditional exporting mm.rdb
+diff -q $D_ECLIPSE/dropins/migdb.mm.rdb_1.0.0.jar $D_MIGDB/plugins/migdb.mm.rdb_1.0.0.jar
 if [ $? -ne 0 ]
   then 
-	echo copying mm rdb
+	echo exporting mm rdb
 	cp $D_MIGDB/plugins/migdb.mm.rdb_1.0.0.jar $D_ECLIPSE/dropins
   else
      echo mm rdb is actual
