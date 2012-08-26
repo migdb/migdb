6525d91cc216cb3d23febd2808d87daec0032a03
woxie <wox2@seznam.cz>
2012-08-26 11:35:26 +0200

    added gupdate script

--- scripts/gpull.sh
+++ scripts/gpull.sh
@@ -1,26 +1,3 @@
 #!/bin/sh
 git pull
-
-#read configuration file
-#. `dirname $0`/config.cfg
-. ~/config.cfg
-
-#conditional copying mm.app
-diff -q plugins/migdb.mm.app_1.0.0.jar $D_ECLIPSE/dropins/migdb.mm.app_1.0.0.jar
-if [ $? -ne 0 ]
-  then 
-	echo copying mm app
-	cp plugins/migdb.mm.app_1.0.0.jar $D_ECLIPSE/dropins
-  else
-     echo mm app is actual
-fi
-
-#conditional copying mm.rdb
-diff -q plugins/migdb.mm.rdb_1.0.0.jar $D_ECLIPSE/dropins/migdb.mm.rdb_1.0.0.jar
-if [ $? -ne 0 ]
-  then 
-	echo copying mm rdb
-	cp plugins/migdb.mm.rdb_1.0.0.jar $D_ECLIPSE/dropins
-  else
-     echo mm rdb is actual
-fi
+`dirname $0`/gupdate.sh
---
+++ scripts/gupdate.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+#read configuration file
+#. `dirname $0`/config.cfg
+. ~/migdb_config.cfg
+
+#conditional copying mm.app
+diff -q plugins/migdb.mm.app_1.0.0.jar $D_ECLIPSE/dropins/migdb.mm.app_1.0.0.jar
+if [ $? -ne 0 ]
+  then 
+	echo copying mm app
+	cp plugins/migdb.mm.app_1.0.0.jar $D_ECLIPSE/dropins
+  else
+     echo mm app is actual
+fi
+
+#conditional copying mm.rdb
+diff -q plugins/migdb.mm.rdb_1.0.0.jar $D_ECLIPSE/dropins/migdb.mm.rdb_1.0.0.jar
+if [ $? -ne 0 ]
+  then 
+	echo copying mm rdb
+	cp plugins/migdb.mm.rdb_1.0.0.jar $D_ECLIPSE/dropins
+  else
+     echo mm rdb is actual
+fi
