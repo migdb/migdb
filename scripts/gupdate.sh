9f10dfd7df55da632cb741ed83d945b1b9f67882
mazanma3 <mazanma3@gmail.com>
2014-02-28 14:23:48 +0100

    Created derived CodeGenComponent, issue #159

--- scripts/gupdate.sh
+++ scripts/gupdate.sh
@@ -6,34 +6,25 @@
 #read configuration file
 . ~/migdb_config.cfg
 
-#conditional copying mm.app
-diff -q $D_MIGDB/plugins/migdb.mm.app_1.0.0.jar $D_ECLIPSE/dropins/migdb.mm.app_1.0.0.jar
-if [ $? -ne 0 ]
-  then 
-	echo copying mm app
-	cp $D_MIGDB/plugins/migdb.mm.app_1.0.0.jar $D_ECLIPSE/dropins
-  else
-     echo mm app is actual
-fi
+#checks plugins version
+checkVer() {
+  diff -q "$D_MIGDB/plugins/$1" "$D_ECLIPSE/dropins/$1"
+  if [ $? -ne 0 ]
+    then 
+    echo "copying $1"
+    cp "$D_MIGDB/plugins/$1" "$D_ECLIPSE/dropins"
+    else
+       echo "$1 app is actual"
+  fi
+}
 
-#conditional copying mm.rdb
-diff -q $D_MIGDB/plugins/migdb.mm.rdb_1.0.0.jar $D_ECLIPSE/dropins/migdb.mm.rdb_1.0.0.jar
-if [ $? -ne 0 ]
-  then 
-	echo copying mm rdb
-	cp $D_MIGDB/plugins/migdb.mm.rdb_1.0.0.jar $D_ECLIPSE/dropins
-  else
-     echo mm rdb is actual
-fi
-
-#conditional copying mm.errors
-diff -q $D_MIGDB/plugins/migdb.mm.errors_1.0.0.jar $D_ECLIPSE/dropins/migdb.mm.errors_1.0.0.jar
-if [ $? -ne 0 ]
-  then 
-	echo copying mm errors
-	cp $D_MIGDB/plugins/migdb.mm.errors_1.0.0.jar $D_ECLIPSE/dropins
-  else
-     echo mm errors is actual
-fi
-
+checkVer migdb.dsl.ide_1.0.0.jar
+checkVer migdb.dsl.ops.ui_1.0.0.jar
+checkVer migdb.dsl.ops_1.0.0.jar
+checkVer migdb.generator_1.0.0.jar
+checkVer migdb.mm.app_1.0.0.jar
+checkVer migdb.mm.errors_1.0.0.jar
+checkVer migdb.mm.rdb_1.0.0.jar
+checkVer migdb.qvto_1.0.0.jar
+checkVer migdb.run_1.0.0.jar
 
