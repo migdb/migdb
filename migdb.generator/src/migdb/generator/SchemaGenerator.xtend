3ed1255cbc9da7ebd2c13708d63d7b027c4dd16c
wox2 <woxie2@gmail.com>
2014-03-26 15:58:20 +0100

    modified SchemaGenerator

--- migdb.generator/src/migdb/generator/SchemaGenerator.xtend
+++ migdb.generator/src/migdb/generator/SchemaGenerator.xtend
@@ -1,12 +1,25 @@
 package migdb.generator
-
+ 
 import eu.collectionspro.mwe.BaseCodeGenerator
 import java.io.PrintWriter
 import org.eclipse.emf.ecore.EObject
 import java.util.ArrayList
+import mm.rdb.Schema
+import mm.rdb.Table
+import mm.rdb.Sequence
+import mm.rdb.Index
+import mm.rdb.Column
 import mm.rdb.impl.TableImpl
-
-
+import mm.rdb.TableConstraint
+import mm.rdb.impl.TableConstraintImpl
+import mm.rdb.impl.IndexImpl
+import mm.rdb.impl.ColumnImpl
+import mm.rdb.impl.SequenceImpl
+import mm.rdb.impl.ForeignKeyImpl
+import mm.rdb.impl.PrimaryKeyImpl
+import mm.rdb.impl.UniqueImpl
+ 
+ 
 class SchemaGenerator extends BaseCodeGenerator {
 	
 	/*****************************************************************
@@ -29,15 +42,15 @@ class SchemaGenerator extends BaseCodeGenerator {
  	  */
 	override doGenerate(EObject model) {
 		this.createWriter
-		var objectList = new ArrayList<TableImpl>();
+		var objectList = new ArrayList<Schema>();
 		
-		for (Object arg : model.eContents) {			
-			if(arg instanceof TableImpl){
-				objectList.add(arg as TableImpl);
+		for (Object arg : model.eContents) {
+			if(arg instanceof Schema){
+				objectList.add(arg as Schema);
 			}
 		}
-		for (TableImpl arg : objectList) {
-			arg.generateOperationFile
+		for (arg : objectList) {
+			arg.generateSchema
 		}
 		writer.close
 	}	
@@ -55,12 +68,27 @@ class SchemaGenerator extends BaseCodeGenerator {
 	 * Method calls the method from the superclass
 	 * Superclass method need filename and SQL text
 	 */
-	def generateOperationFile(TableImpl arg) {
-		var text = arg.genSQL;
-		this.write(text);
+	def generateSchema(Schema arg) {
+		this.write('');
+		
+		var constraints = new ArrayList<TableConstraint>();
+		
+		for (EObject object : arg.eContents){
+			if(object instanceof Table){
+				this.write(object.genSQL)
+				val Table table = object as Table;
+				for(TableConstraint tableconstraint : table.constraints){
+					constraints.add(tableconstraint);
+				}				
+			}	
+		}
+
+		for(TableConstraint constraint : constraints){
+			this.write(constraint.genSQL)
+		}
 	}
 	
-		/**
+	/**
 	 * Writes string to opened writer
 	 * @param CharSequence text : string to be written
 	 */
@@ -76,7 +104,44 @@ class SchemaGenerator extends BaseCodeGenerator {
 		this.filename = path;
 	}
 	
-	def genSQL(TableImpl arg)'''
-		1
+	def dispatch genSQL(Table table) '''
+		CREATE TABLE «table.owningSchema.name».«table.name» (
+			«FOR column : table.columns SEPARATOR ","»
+				«column.genSQL»
+			«ENDFOR»
+		);
+	'''
+	
+	def dispatch genSQL(SequenceImpl sequence)'''
+		CREATE SEQUENCE «sequence.name» START «sequence.startValue»;
+	'''
+	
+	def dispatch genSQL(IndexImpl index)'''
+		CREATE INDEX «index.name» ON «index.columns.get(0).owningTable.name» (
+			«FOR column : index.columns SEPARATOR ","»
+				«column.name»
+			«ENDFOR»
+		);
+	'''
+	
+	def dispatch genSQL(ColumnImpl column) '''
+		«column.name» «column.type.name» «IF column.defaultValue != null» DEFAULT «column.defaultValue»«ENDIF»«IF !column.isNillable» NOT NULL «ENDIF»
+	'''
+	
+	def dispatch genSQL(ForeignKeyImpl foreignKey)'''
+		ALTER TABLE «foreignKey.owningTable.name» ADD CONSTRAINT «foreignKey.name»
+		   FOREIGN KEY («foreignKey.constrainedColumn.name») 
+		        REFERENCES «foreignKey.targetTable.name»(id_«foreignKey.targetTable.name»);
+	'''
+
+	def dispatch genSQL(PrimaryKeyImpl primaryKey)'''
+		ALTER TABLE «primaryKey.owningTable.name» ADD CONSTRAINT «primaryKey.name» PRIMARY KEY («primaryKey.constrainedColumn.name»);
+	'''
+
+	def dispatch genSQL(UniqueImpl unique)'''
+		ALTER TABLE «unique.owningTable.name» ADD CONSTRAINT «unique.name» 
+			UNIQUE («FOR column : unique.uniqueColumns SEPARATOR ","»
+				«column.name»
+			«ENDFOR»);
 	'''
 }
--- migdb.generator/xtend-gen/migdb/generator/SchemaGenerator.java
+++ migdb.generator/xtend-gen/migdb/generator/SchemaGenerator.java
@@ -4,11 +4,24 @@ import eu.collectionspro.mwe.BaseCodeGenerator;
 import java.io.File;
 import java.io.PrintWriter;
 import java.util.ArrayList;
-import mm.rdb.impl.TableImpl;
+import java.util.Arrays;
+import mm.rdb.Column;
+import mm.rdb.PrimitiveType;
+import mm.rdb.Schema;
+import mm.rdb.Table;
+import mm.rdb.TableConstraint;
+import mm.rdb.impl.ColumnImpl;
+import mm.rdb.impl.ForeignKeyImpl;
+import mm.rdb.impl.IndexImpl;
+import mm.rdb.impl.PrimaryKeyImpl;
+import mm.rdb.impl.SequenceImpl;
+import mm.rdb.impl.UniqueImpl;
 import org.eclipse.emf.common.util.EList;
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.xtend2.lib.StringConcatenation;
+import org.eclipse.xtext.xbase.lib.BooleanExtensions;
 import org.eclipse.xtext.xbase.lib.Exceptions;
+import org.eclipse.xtext.xbase.lib.ObjectExtensions;
 
 @SuppressWarnings("all")
 public class SchemaGenerator extends BaseCodeGenerator {
@@ -27,16 +40,16 @@ public class SchemaGenerator extends BaseCodeGenerator {
    */
   public void doGenerate(final EObject model) {
       this.createWriter();
-      ArrayList<TableImpl> _arrayList = new ArrayList<TableImpl>();
-      ArrayList<TableImpl> objectList = _arrayList;
+      ArrayList<Schema> _arrayList = new ArrayList<Schema>();
+      ArrayList<Schema> objectList = _arrayList;
       EList<EObject> _eContents = model.eContents();
       for (final Object arg : _eContents) {
-        if ((arg instanceof TableImpl)) {
-          objectList.add(((TableImpl) arg));
+        if ((arg instanceof Schema)) {
+          objectList.add(((Schema) arg));
         }
       }
-      for (final TableImpl arg_1 : objectList) {
-        this.generateOperationFile(arg_1);
+      for (final Schema arg_1 : objectList) {
+        this.generateSchema(arg_1);
       }
       this.writer.close();
   }
@@ -64,10 +77,28 @@ public class SchemaGenerator extends BaseCodeGenerator {
    * Method calls the method from the superclass
    * Superclass method need filename and SQL text
    */
-  public void generateOperationFile(final TableImpl arg) {
-      CharSequence _genSQL = this.genSQL(arg);
-      CharSequence text = _genSQL;
-      this.write(text);
+  public void generateSchema(final Schema arg) {
+      this.write("");
+      ArrayList<TableConstraint> _arrayList = new ArrayList<TableConstraint>();
+      ArrayList<TableConstraint> constraints = _arrayList;
+      EList<EObject> _eContents = arg.eContents();
+      for (final EObject object : _eContents) {
+        if ((object instanceof Table)) {
+          {
+            CharSequence _genSQL = this.genSQL(object);
+            this.write(_genSQL);
+            final Table table = ((Table) object);
+            EList<TableConstraint> _constraints = table.getConstraints();
+            for (final TableConstraint tableconstraint : _constraints) {
+              constraints.add(tableconstraint);
+            }
+          }
+        }
+      }
+      for (final TableConstraint constraint : constraints) {
+        CharSequence _genSQL_1 = this.genSQL(constraint);
+        this.write(_genSQL_1);
+      }
   }
   
   /**
@@ -87,10 +118,214 @@ public class SchemaGenerator extends BaseCodeGenerator {
     return _filename;
   }
   
-  public CharSequence genSQL(final TableImpl arg) {
+  protected CharSequence _genSQL(final Table table) {
     StringConcatenation _builder = new StringConcatenation();
-    _builder.append("1");
+    _builder.append("CREATE TABLE ");
+    Schema _owningSchema = table.getOwningSchema();
+    String _name = _owningSchema.getName();
+    _builder.append(_name, "");
+    _builder.append(".");
+    String _name_1 = table.getName();
+    _builder.append(_name_1, "");
+    _builder.append(" (");
+    _builder.newLineIfNotEmpty();
+    {
+      EList<Column> _columns = table.getColumns();
+      boolean _hasElements = false;
+      for(final Column column : _columns) {
+        if (!_hasElements) {
+          _hasElements = true;
+        } else {
+          _builder.appendImmediate(",", "	");
+        }
+        _builder.append("\t");
+        CharSequence _genSQL = this.genSQL(column);
+        _builder.append(_genSQL, "	");
+        _builder.newLineIfNotEmpty();
+      }
+    }
+    _builder.append(");");
     _builder.newLine();
     return _builder;
+  }
+  
+  protected CharSequence _genSQL(final SequenceImpl sequence) {
+    StringConcatenation _builder = new StringConcatenation();
+    _builder.append("CREATE SEQUENCE ");
+    String _name = sequence.getName();
+    _builder.append(_name, "");
+    _builder.append(" START ");
+    int _startValue = sequence.getStartValue();
+    _builder.append(_startValue, "");
+    _builder.append(";");
+    _builder.newLineIfNotEmpty();
+    return _builder;
+  }
+  
+  protected CharSequence _genSQL(final IndexImpl index) {
+    StringConcatenation _builder = new StringConcatenation();
+    _builder.append("CREATE INDEX ");
+    String _name = index.getName();
+    _builder.append(_name, "");
+    _builder.append(" ON ");
+    EList<Column> _columns = index.getColumns();
+    Column _get = _columns.get(0);
+    Table _owningTable = _get.getOwningTable();
+    String _name_1 = _owningTable.getName();
+    _builder.append(_name_1, "");
+    _builder.append(" (");
+    _builder.newLineIfNotEmpty();
+    {
+      EList<Column> _columns_1 = index.getColumns();
+      boolean _hasElements = false;
+      for(final Column column : _columns_1) {
+        if (!_hasElements) {
+          _hasElements = true;
+        } else {
+          _builder.appendImmediate(",", "	");
+        }
+        _builder.append("\t");
+        String _name_2 = column.getName();
+        _builder.append(_name_2, "	");
+        _builder.newLineIfNotEmpty();
+      }
+    }
+    _builder.append(");");
+    _builder.newLine();
+    return _builder;
+  }
+  
+  protected CharSequence _genSQL(final ColumnImpl column) {
+    StringConcatenation _builder = new StringConcatenation();
+    String _name = column.getName();
+    _builder.append(_name, "");
+    _builder.append(" ");
+    PrimitiveType _type = column.getType();
+    String _name_1 = _type.name();
+    _builder.append(_name_1, "");
+    _builder.append(" ");
+    {
+      String _defaultValue = column.getDefaultValue();
+      boolean _operator_notEquals = ObjectExtensions.operator_notEquals(_defaultValue, null);
+      if (_operator_notEquals) {
+        _builder.append(" DEFAULT ");
+        String _defaultValue_1 = column.getDefaultValue();
+        _builder.append(_defaultValue_1, "");
+      }
+    }
+    {
+      Boolean _isNillable = column.getIsNillable();
+      boolean _operator_not = BooleanExtensions.operator_not(_isNillable);
+      if (_operator_not) {
+        _builder.append(" NOT NULL ");
+      }
+    }
+    _builder.newLineIfNotEmpty();
+    return _builder;
+  }
+  
+  protected CharSequence _genSQL(final ForeignKeyImpl foreignKey) {
+    StringConcatenation _builder = new StringConcatenation();
+    _builder.append("ALTER TABLE ");
+    Table _owningTable = foreignKey.getOwningTable();
+    String _name = _owningTable.getName();
+    _builder.append(_name, "");
+    _builder.append(" ADD CONSTRAINT ");
+    String _name_1 = foreignKey.getName();
+    _builder.append(_name_1, "");
+    _builder.newLineIfNotEmpty();
+    _builder.append("   ");
+    _builder.append("FOREIGN KEY (");
+    Column _constrainedColumn = foreignKey.getConstrainedColumn();
+    String _name_2 = _constrainedColumn.getName();
+    _builder.append(_name_2, "   ");
+    _builder.append(") ");
+    _builder.newLineIfNotEmpty();
+    _builder.append("        ");
+    _builder.append("REFERENCES ");
+    Table _targetTable = foreignKey.getTargetTable();
+    String _name_3 = _targetTable.getName();
+    _builder.append(_name_3, "        ");
+    _builder.append("(id_");
+    Table _targetTable_1 = foreignKey.getTargetTable();
+    String _name_4 = _targetTable_1.getName();
+    _builder.append(_name_4, "        ");
+    _builder.append(");");
+    _builder.newLineIfNotEmpty();
+    return _builder;
+  }
+  
+  protected CharSequence _genSQL(final PrimaryKeyImpl primaryKey) {
+    StringConcatenation _builder = new StringConcatenation();
+    _builder.append("ALTER TABLE ");
+    Table _owningTable = primaryKey.getOwningTable();
+    String _name = _owningTable.getName();
+    _builder.append(_name, "");
+    _builder.append(" ADD CONSTRAINT ");
+    String _name_1 = primaryKey.getName();
+    _builder.append(_name_1, "");
+    _builder.append(" PRIMARY KEY (");
+    Column _constrainedColumn = primaryKey.getConstrainedColumn();
+    String _name_2 = _constrainedColumn.getName();
+    _builder.append(_name_2, "");
+    _builder.append(");");
+    _builder.newLineIfNotEmpty();
+    return _builder;
+  }
+  
+  protected CharSequence _genSQL(final UniqueImpl unique) {
+    StringConcatenation _builder = new StringConcatenation();
+    _builder.append("ALTER TABLE ");
+    Table _owningTable = unique.getOwningTable();
+    String _name = _owningTable.getName();
+    _builder.append(_name, "");
+    _builder.append(" ADD CONSTRAINT ");
+    String _name_1 = unique.getName();
+    _builder.append(_name_1, "");
+    _builder.append(" ");
+    _builder.newLineIfNotEmpty();
+    _builder.append("\t");
+    _builder.append("UNIQUE (");
+    {
+      EList<Column> _uniqueColumns = unique.getUniqueColumns();
+      boolean _hasElements = false;
+      for(final Column column : _uniqueColumns) {
+        if (!_hasElements) {
+          _hasElements = true;
+        } else {
+          _builder.appendImmediate(",", "	");
+        }
+        _builder.newLineIfNotEmpty();
+        _builder.append("\t");
+        String _name_2 = column.getName();
+        _builder.append(_name_2, "	");
+        _builder.newLineIfNotEmpty();
+        _builder.append("\t\t\t");
+      }
+    }
+    _builder.append(");");
+    _builder.newLineIfNotEmpty();
+    return _builder;
+  }
+  
+  public CharSequence genSQL(final EObject foreignKey) {
+    if (foreignKey instanceof ForeignKeyImpl) {
+      return _genSQL((ForeignKeyImpl)foreignKey);
+    } else if (foreignKey instanceof PrimaryKeyImpl) {
+      return _genSQL((PrimaryKeyImpl)foreignKey);
+    } else if (foreignKey instanceof UniqueImpl) {
+      return _genSQL((UniqueImpl)foreignKey);
+    } else if (foreignKey instanceof ColumnImpl) {
+      return _genSQL((ColumnImpl)foreignKey);
+    } else if (foreignKey instanceof IndexImpl) {
+      return _genSQL((IndexImpl)foreignKey);
+    } else if (foreignKey instanceof SequenceImpl) {
+      return _genSQL((SequenceImpl)foreignKey);
+    } else if (foreignKey instanceof Table) {
+      return _genSQL((Table)foreignKey);
+    } else {
+      throw new IllegalArgumentException("Unhandled parameter types: " +
+        Arrays.<Object>asList(foreignKey).toString());
+    }
   }
 }
